095adbbd9eecf2fe0bf282adbe3e68daa4db57d2
gimura2022 <gimura0001@gmail.com>
2024-11-17 02:27:50 +0300

    chanded self recompile example

--- examples/gnub_recompile_self.c
+++ examples/gnub_recompile_self.c
@@ -3,8 +3,7 @@
 int main(int argc, char* argv[])
 {
 	struct gnub__cmd_arr self_compile = {0};
-	gnub__compile_c(&self_compile, "cc", "-std=c99 -Wall -g", "", "", "examples/gnub_recompile_self.c",
-			"gnub.new");
+	gnub__compile_c(&self_compile, "cc", "", "", "", "examples/gnub_recompile_self.c", "gnub.new");
 	gnub__recompile_self(&self_compile, "gnub.new", argv);
 	gnub__free_commands(&self_compile);
 
