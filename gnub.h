ff39600aca0049261514b250cbf05fe945857038
gimura2022 <gimura0001@gmail.com>
2024-11-16 23:36:01 +0300

    added new way to add commands

--- gnub.h
+++ gnub.h
@@ -30,15 +30,29 @@ struct gnub__cmd_arr {
 	struct _gnub__cmd* end;
 };
 
+/* private functions */
+
 void _gnub__append_command(struct gnub__cmd_arr* arr, const size_t count, const char** parts);
+void _gnub__append_parts_to_last(struct gnub__cmd_arr* arr, const size_t count, const char** parts);
+void _gnub__append_parts_by_index(struct gnub__cmd_arr* arr, const size_t count, const char** parts);
+void _gnub__append_to_command(struct _gnub__cmd* cmd, const size_t count, const char** parts);
+
 void _gnub__execute_command(struct _gnub__cmd* cmd);
 void _gnub__free_command(struct _gnub__cmd* cmd);
+
+/* public functions */
 
 void gnub__execute_commands(struct gnub__cmd_arr* cmds);
 void gnub__free_commands(struct gnub__cmd_arr* cmds);
 
-#define gnub__append_command(arr, ...) ({ const char* __parts[] = {__VA_ARGS__}; \
-		_gnub__append_command(arr, array_lenght(__parts), __parts); })
+#define _gnub__parts_command(x, arr, ...) ({ const char* __parts[] = {__VA_ARGS__}; \
+		x(arr, array_lenght(__parts), __parts); })
+
+#define gnub__append_command(arr, ...) _gnub__parts_command(_gnub__append_command, arr, __VA_ARGS__)
+#define gnub__append_parts_to_last(arr, ...) _gnub__parts_command(_gnub__append_parts_to_last, \
+		arr, __VA_ARGS__)
+#define gnub__append_parts_by_index(arr, ...) _gnub__parts_command(_gnub__append_parts_by_index, \
+		arr, __VA_ARGS__)
 
 #endif
 
@@ -46,6 +60,21 @@ void gnub__free_commands(struct gnub__cmd_arr* cmds);
 
 #ifndef _gnub_impl
 #define _gnub_impl
+
+/* private functions */
+
+void _gnub__append_to_command(struct _gnub__cmd* cmd, const size_t count, const char** parts)
+{
+	cmd->start = (struct _gnub__cmd_part*) malloc(sizeof(struct _gnub__cmd_part));
+	cmd->end   = cmd->start;
+
+	for (size_t i = 0; i < count; i++) {
+		memcpy(cmd->end, parts[i], strlen(parts[i]));
+
+		cmd->end->next = (struct _gnub__cmd_part*) malloc(sizeof(struct _gnub__cmd_part));
+		cmd->end = cmd->end->next;
+	}
+}
 
 void _gnub__append_command(struct gnub__cmd_arr* arr, const size_t count, const char** parts)
 {
@@ -60,18 +89,10 @@ void _gnub__append_command(struct gnub__cmd_arr* arr, const size_t count, const 
 
 	if (!is_first_cmd) {
 		arr->end->next = (struct _gnub__cmd*) malloc(sizeof(struct _gnub__cmd));
-		arr->end = arr->end->next;
+		arr->end       = arr->end->next;
 	}
 
-	arr->end->start = (struct _gnub__cmd_part*) malloc(sizeof(struct _gnub__cmd_part));
-	arr->end->end = arr->end->start;
-
-	for (size_t i = 0; i < count; i++) {
-		memcpy(arr->end->end->str, parts[i], strlen(parts[i]));
-
-		arr->end->end->next = (struct _gnub__cmd_part*) malloc(sizeof(struct _gnub__cmd_part));
-		arr->end->end = arr->end->end->next;
-	}
+	_gnub__append_to_command(arr->end, count, parts);
 }
 
 void _gnub__execute_command(struct _gnub__cmd* cmd)
@@ -101,6 +122,25 @@ void _gnub__free_command(struct _gnub__cmd* cmd)
 
 	free(cmd);
 }
+
+void _gnub__append_parts_to_last(struct gnub__cmd_arr* arr, const size_t count, const char** parts)
+{
+	_gnub__append_to_command(arr->end, count, parts); // TODO: add check to empty command array
+}
+
+void _gnub__append_parts_by_index(struct gnub__cmd_arr* arr, const size_t count, const char** parts)
+{
+	size_t i               = 0;
+	struct _gnub__cmd* cmd = arr->start;
+	while (i < count && cmd != NULL) {
+		i++;
+		cmd = cmd->next;
+	}
+
+	_gnub__append_to_command(cmd, count, parts);
+}
+
+/* public functions */
 
 void gnub__execute_commands(struct gnub__cmd_arr* cmds)
 {
